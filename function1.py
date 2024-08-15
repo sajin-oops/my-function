1e2908a588a097b02501f6d2a1803a6d6812979d
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-15 21:06:00 +0530

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,30 @@
+def myfun(a,b): # name = parameter
+  print("Hello " + a)
+  print("Welcome to "+ b)
+
+
+myfun("Sajin","singapore") # sajin = passing arguments
+
+'''
+
+Hello Sajin
+Welcome to singapore
+Hello Lijin
+Welcome to Germany
+
+'''
+myfun("Lijin","Germany")
+
+def fun(c,d): # parameter
+  print("Congratulation " + c)
+  print("You won a brand new " +d )
+  
+customer = "Sajin"
+car = "BMW"
+fun(customer,car)
+  
+''''
+O/P 
+Congratulation Sajin
+You won a brand new BMW
+'''
